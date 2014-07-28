41d9e02305e6fe69706d3b1be2734d237be25863
Scott Doxey <neogeek0101@gmail.com>
2014-07-28 18:36:26 -0400

    Added server-side Handlebars support.

--- web.js
+++ web.js
@@ -5,4 +5,17 @@ var app = express();
 
 app.use(express.static(__dirname + '/static'));
 
+app.engine('hbs', require('express-hbs').express3());
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/src/views');
+
+app.get('/', function (res, res) {
+
+    res.render('index', {
+        title: 'Node.js Starter Kit'
+    });
+
+});
+
 app.listen(process.env.PORT || 5000);
