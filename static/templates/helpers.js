ff2151f9dd6aa288e055b1ec17ea2c9ce2ffbd03
Scott Doxey <neogeek0101@gmail.com>
2014-07-29 19:45:15 -0400

    Handlebars helpers work both client/server side.

--- static/templates/helpers.js
+++ static/templates/helpers.js
@@ -1,11 +1,27 @@
-define(['handlebars'], function (Handlebars) {
+(function (root, factory) {
+
+    'use strict';
+
+    if (typeof define === 'function' && define.amd !== undefined) {
+
+        define(['handlebars'], factory);
+
+    } else if (typeof module === 'object' && module.exports !== undefined) {
+
+        module.exports = factory;
+
+    }
+
+}(null, function (Handlebars) {
 
     'use strict';
 
     var helpers = {
+
         ifCond: function (a, b, options) {
             return a === b ? options.fn(this) : options.inverse(this);
         }
+
     };
 
     Object.keys(helpers).forEach(function (key) {
@@ -14,4 +30,4 @@ define(['handlebars'], function (Handlebars) {
 
     });
 
-});
+}));
--- web.js
+++ web.js
@@ -3,9 +3,12 @@
 var express = require('express');
 var app = express();
 
+var hbs = require('express-hbs');
+var hbs_helpers = require('./static/templates/helpers.js')(hbs);
+
 app.use(express.static(__dirname + '/static'));
 
-app.engine('hbs', require('express-hbs').express3());
+app.engine('hbs', hbs.express3());
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/src/views');
