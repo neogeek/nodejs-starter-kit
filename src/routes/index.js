e8487126fa92f4c0371ba5f7473ed9f1c28c1e0c
Scott Doxey <neogeek0101@gmail.com>
2014-09-14 19:25:34 -0400

    Added express-enrouten package for route support.
    Updated packages.

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+module.exports = function (router) {
+
+    router.get('/', function (req, res) {
+
+        res.render('index', {
+            title: 'Node.js Starter Kit'
+        });
+
+    });
+
+};
--- web.js
+++ web.js
@@ -2,23 +2,18 @@
 
 var express = require('express');
 var app = express();
+var enrouten = require('express-enrouten');
 
 var hbs = require('express-hbs');
 var hbs_helpers = require('./static/templates/helpers.js')(hbs);
 
 app.use(express.static(__dirname + '/static'));
 
+app.use(enrouten({ directory: 'src/routes' }));
+
 app.engine('hbs', hbs.express3());
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/src/views');
-
-app.get('/', function (req, res) {
-
-    res.render('index', {
-        title: 'Node.js Starter Kit'
-    });
-
-});
 
 app.listen(process.env.PORT || 5000);
