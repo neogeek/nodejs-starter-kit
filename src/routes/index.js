37c4cf81ec2559ab8f80610522c0c3d5272e352e
Scott Doxey <neogeek0101@gmail.com>
2015-01-01 18:49:39 -0500

    Setup proper templates.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,16 @@ var indexView = require('../views/index');
 
 module.exports = function (router) {
 
+    router.get('/*', function (req, res, next) {
+
+        res.locals = {
+            layout: 'template'
+        };
+
+        next();
+
+    });
+
     router.get('/', indexView);
 
 };
