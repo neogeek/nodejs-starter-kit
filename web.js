87b8b04ad625ef658d8dc17954563d09f28a1f2b
Scott Doxey <neogeek0101@gmail.com>
2015-01-01 20:39:58 -0500

    Added session and body-parser packages.

--- web.js
+++ web.js
@@ -2,12 +2,23 @@
 
 var express = require('express');
 var app = express();
+
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var enrouten = require('express-enrouten');
 
 var hbs = require('express-hbs');
 var hbs_helpers = require('./static/templates/helpers.js')(hbs);
 
 app.use(express.static(__dirname + '/static'));
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(enrouten({ directory: 'src/routes' }));
 
