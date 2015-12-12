4a742d91f91d06ec119967d61c976dc775fe5b79
Scott Doxey <neogeek0101@gmail.com>
2015-12-12 09:07:36 -0500

    Added example usage of Handlebars to client side app script.

--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,12 @@ define(function (require) {
 
     'use strict';
 
+    var $ = require('jquery'),
+        Handlebars = require('handlebars'),
+        templates = require('templates');
+
+    templates.partials = Handlebars.partials;
+
+    console.log(templates);
+
 });
