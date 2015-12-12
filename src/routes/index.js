3a5f5f740a7d6da9a042fe6bc55443650e881739
Scott Doxey <neogeek0101@gmail.com>
2015-12-12 09:07:04 -0500

    Added localized title to intro page.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
 
-        res.render('index');
+        res.render('index', {
+            page_title: req.__('Intro')
+        });
 
     });
 
