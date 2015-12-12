079f0675b17c60d214a8e09791e48a8aaffb7fbb
Scott Doxey <neogeek0101@gmail.com>
2015-12-12 08:59:45 -0500

    Removed unused key/value pairs.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,7 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
 
-        res.render('index', {
-            title: 'Node.js Starter Kit',
-            content: 'Sample Handlebars Template'
-        });
+        res.render('index');
 
     });
 
