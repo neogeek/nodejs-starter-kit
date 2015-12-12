2d6ec55e10945e14bb60f09542a5ddfbf44e0494
Scott Doxey <neogeek0101@gmail.com>
2015-12-12 09:00:16 -0500

    Removed sample template.
    Emptied client side JavaScript file.

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,4 @@ define(function (require) {
 
     'use strict';
 
-    var $ = require('jquery'),
-        templates = require('templates');
-
-    $('body').append(templates.sample({ content: 'Sample Handlebars Template' }));
-
 });
